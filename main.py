de33170900b9fa76626f7809c8a7f51908a9291d
joseph262 <153666891+joseph262@users.noreply.github.com>
2024-01-03 09:32:07 -0600

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ _steps = [
 
 # This automatically reads in the configuration
 @hydra.main(config_name='config')
-def go(config: DictConfig):
+def hd (config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
@@ -63,30 +63,31 @@ def go(config: DictConfig):
      )
 
         if "data_check" in active_steps:
-            _ = mlflow.run(
-                uri=os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
-                entry_point="main",
+            __ = mlflow.run(
+                os.path.join(
+                    hydra.utils.get_original_cwd(),
+                    "src",
+                    "data_check"),
+                "main",
                 parameters={
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
                     "kl_threshold": config["data_check"]["kl_threshold"],
                     "min_price": config["etl"]["min_price"],
-                    "max_price": config["etl"]["max_price"],
-                },
+                    "max_price": config["etl"]["max_price"]},
             )
 
-        if "data_split" in active_steps:
-            _ = mlflow.run(
-                uri=f"{config['main']['components_repository']}/train_val_test_split",
-                entry_point="main",
+            if "data_split" in active_steps:
+                _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
                 parameters={
                     "input": "clean_sample.csv:latest",
                     "test_size": config["modeling"]["test_size"],
                     "random_seed": config["modeling"]["random_seed"],
-                    "stratify": config["modeling"]["stratify_by"],
-                },
-            )
-
+                    "stratify": config["modeling"]["stratify_by"]},
+                )
+                
         if "train_random_forest" in active_steps:
 
             # NOTE: we need to serialize the random forest configuration into JSON
@@ -96,11 +97,12 @@ def go(config: DictConfig):
 
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
-
-            ##################
             _ = mlflow.run(
-                uri=os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
-                entry_point="main",
+                os.path.join(
+                    hydra.utils.get_original_cwd(),
+                    "src",
+                    "train_random_forest"),
+                "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
                     "val_size": config["modeling"]["val_size"],
@@ -108,26 +110,24 @@ def go(config: DictConfig):
                     "stratify_by": config["modeling"]["stratify_by"],
                     "rf_config": rf_config,
                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                    "output_artifact": "random_forest_export",
-                },
+                    "output_artifact": "random_forest_export"},
             )
-           
-            ##################
+            
 
 
         if "test_regression_model" in active_steps:
                _ = mlflow.run(
                 f"{config['main']['components_repository']}/test_regression_model",
-                entry_point="main",
+                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
                     "test_dataset": "test_data.csv:latest",
                 },
             )
-            ##################
+          
 
           
 
 
-        if __name__ == "__main__":
-            go()
+if __name__ == "__main__":
+     hd()
